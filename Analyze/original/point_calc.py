a01423290bbb7798cde2c8eda0032977125e5fe5
Y-D-Lu <e4256318151@gmail.com>
2018-04-14 00:10:43 +0800

    Make it looks better

--- Analyze/original/point_calc.py
+++ Analyze/original/point_calc.py
@@ -178,163 +178,163 @@ def calc(p, do_round=True):
 def df_calc(p):
     p = p.copy()
 
-    p['ovaGK'] = .11 * p['Reactions'] + \
-                 .21 * p['GK_Diving'] + \
-                 .21 * p['GK_Handling'] + \
-                 .05 * p['GK_Kicking'] + \
-                 .21 * p['GK_Positioning'] + \
-                 .21 * p['GK_Reflexes']
+    p['ovaGK'] = (.11 * p['Reactions'] +
+                  .21 * p['GK_Diving'] +
+                  .21 * p['GK_Handling'] +
+                  .05 * p['GK_Kicking'] +
+                  .21 * p['GK_Positioning'] +
+                  .21 * p['GK_Reflexes'])
 
-    p['ovaWB'] = .04 * p['Acceleration'] + \
-                 .06 * p['Sprint_Speed'] + \
-                 .1 * p['Stamina'] + \
-                 .08 * p['Reactions'] + \
-                 .12 * p['Interceptions'] + \
-                 .08 * p['Ball_Control'] + \
-                 .12 * p['Crossing'] + \
-                 .04 * p['Dribbling'] + \
-                 .1 * p['Short_Passing'] + \
-                 .07 * p['Marking'] + \
-                 .08 * p['Standing_Tackle'] + \
-                 .11 * p['Sliding_Tackle']
+    p['ovaWB'] = (.04 * p['Acceleration'] +
+                  .06 * p['Sprint_Speed'] +
+                  .1 * p['Stamina'] +
+                  .08 * p['Reactions'] +
+                  .12 * p['Interceptions'] +
+                  .08 * p['Ball_Control'] +
+                  .12 * p['Crossing'] +
+                  .04 * p['Dribbling'] +
+                  .1 * p['Short_Passing'] +
+                  .07 * p['Marking'] +
+                  .08 * p['Standing_Tackle'] +
+                  .11 * p['Sliding_Tackle'])
 
-    p['ovaFB'] = .05 * p['Acceleration'] + \
-                 .07 * p['Sprint_Speed'] + \
-                 .08 * p['Stamina'] + \
-                 .08 * p['Reactions'] + \
-                 .12 * p['Interceptions'] + \
-                 .07 * p['Ball_Control'] + \
-                 .09 * p['Crossing'] + \
-                 .04 * p['Heading_Accuracy'] + \
-                 .07 * p['Short_Passing'] + \
-                 .08 * p['Marking'] + \
-                 .11 * p['Standing_Tackle'] + \
-                 .14 * p['Sliding_Tackle']
+    p['ovaFB'] = (.05 * p['Acceleration'] +
+                  .07 * p['Sprint_Speed'] +
+                  .08 * p['Stamina'] +
+                  .08 * p['Reactions'] +
+                  .12 * p['Interceptions'] +
+                  .07 * p['Ball_Control'] +
+                  .09 * p['Crossing'] +
+                  .04 * p['Heading_Accuracy'] +
+                  .07 * p['Short_Passing'] +
+                  .08 * p['Marking'] +
+                  .11 * p['Standing_Tackle'] +
+                  .14 * p['Sliding_Tackle'])
 
-    p['ovaCB'] = .02 * p['Sprint_Speed'] + \
-                 .03 * p['Jumping'] + \
-                 .1 * p['Strength'] + \
-                 .05 * p['Reactions'] + \
-                 .07 * p['Aggression'] + \
-                 .13 * p['Interceptions'] + \
-                 .04 * p['Ball_Control'] + \
-                 .1 * p['Heading_Accuracy'] + \
-                 .05 * p['Short_Passing'] + \
-                 .14 * p['Marking'] + \
-                 .17 * p['Standing_Tackle'] + \
-                 .1 * p['Sliding_Tackle']
+    p['ovaCB'] = (.02 * p['Sprint_Speed'] +
+                  .03 * p['Jumping'] +
+                  .1 * p['Strength'] +
+                  .05 * p['Reactions'] +
+                  .07 * p['Aggression'] +
+                  .13 * p['Interceptions'] +
+                  .04 * p['Ball_Control'] +
+                  .1 * p['Heading_Accuracy'] +
+                  .05 * p['Short_Passing'] +
+                  .14 * p['Marking'] +
+                  .17 * p['Standing_Tackle'] +
+                  .1 * p['Sliding_Tackle'])
 
-    p['ovaDM'] = .06 * p['Stamina'] + \
-                 .04 * p['Strength'] + \
-                 .07 * p['Reactions'] + \
-                 .05 * p['Aggression'] + \
-                 .14 * p['Interceptions'] + \
-                 .04 * p['Vision'] + \
-                 .1 * p['Ball_Control'] + \
-                 .1 * p['Long_Passing'] + \
-                 .14 * p['Short_Passing'] + \
-                 .09 * p['Marking'] + \
-                 .12 * p['Standing_Tackle'] + \
-                 .05 * p['Sliding_Tackle']
+    p['ovaDM'] = (.06 * p['Stamina'] +
+                  .04 * p['Strength'] +
+                  .07 * p['Reactions'] +
+                  .05 * p['Aggression'] +
+                  .14 * p['Interceptions'] +
+                  .04 * p['Vision'] +
+                  .1 * p['Ball_Control'] +
+                  .1 * p['Long_Passing'] +
+                  .14 * p['Short_Passing'] +
+                  .09 * p['Marking'] +
+                  .12 * p['Standing_Tackle'] +
+                  .05 * p['Sliding_Tackle'])
 
-    p['ovaWM'] = .07 * p['Acceleration'] + \
-                 .06 * p['Sprint_Speed'] + \
-                 .05 * p['Stamina'] + \
-                 .07 * p['Reactions'] + \
-                 .08 * p['Positioning'] + \
-                 .07 * p['Vision'] + \
-                 .13 * p['Ball_Control'] + \
-                 .1 * p['Crossing'] + \
-                 .15 * p['Dribbling'] + \
-                 .06 * p['Finishing'] + \
-                 .05 * p['Long_Passing'] + \
-                 .11 * p['Short_Passing']
+    p['ovaWM'] = (.07 * p['Acceleration'] +
+                  .06 * p['Sprint_Speed'] +
+                  .05 * p['Stamina'] +
+                  .07 * p['Reactions'] +
+                  .08 * p['Positioning'] +
+                  .07 * p['Vision'] +
+                  .13 * p['Ball_Control'] +
+                  .1 * p['Crossing'] +
+                  .15 * p['Dribbling'] +
+                  .06 * p['Finishing'] +
+                  .05 * p['Long_Passing'] +
+                  .11 * p['Short_Passing'])
 
-    p['ovaCM'] = .06 * p['Stamina'] + \
-                 .08 * p['Reactions'] + \
-                 .05 * p['Interceptions'] + \
-                 .06 * p['Positioning'] + \
-                 .13 * p['Vision'] + \
-                 .14 * p['Ball_Control'] + \
-                 .07 * p['Dribbling'] + \
-                 .02 * p['Finishing'] + \
-                 .13 * p['Long_Passing'] + \
-                 .17 * p['Short_Passing'] + \
-                 .04 * p['Long_Shots'] + \
-                 .05 * p['Standing_Tackle']
+    p['ovaCM'] = (.06 * p['Stamina'] +
+                  .08 * p['Reactions'] +
+                  .05 * p['Interceptions'] +
+                  .06 * p['Positioning'] +
+                  .13 * p['Vision'] +
+                  .14 * p['Ball_Control'] +
+                  .07 * p['Dribbling'] +
+                  .02 * p['Finishing'] +
+                  .13 * p['Long_Passing'] +
+                  .17 * p['Short_Passing'] +
+                  .04 * p['Long_Shots'] +
+                  .05 * p['Standing_Tackle'])
 
-    p['ovaAM'] = .04 * p['Acceleration'] + \
-                 .03 * p['Sprint_Speed'] + \
-                 .03 * p['Agility'] + \
-                 .07 * p['Reactions'] + \
-                 .09 * p['Positioning'] + \
-                 .14 * p['Vision'] + \
-                 .15 * p['Ball_Control'] + \
-                 .13 * p['Dribbling'] + \
-                 .07 * p['Finishing'] + \
-                 .04 * p['Long_Passing'] + \
-                 .16 * p['Short_Passing'] + \
-                 .05 * p['Long_Shots']
+    p['ovaAM'] = (.04 * p['Acceleration'] +
+                  .03 * p['Sprint_Speed'] +
+                  .03 * p['Agility'] +
+                  .07 * p['Reactions'] +
+                  .09 * p['Positioning'] +
+                  .14 * p['Vision'] +
+                  .15 * p['Ball_Control'] +
+                  .13 * p['Dribbling'] +
+                  .07 * p['Finishing'] +
+                  .04 * p['Long_Passing'] +
+                  .16 * p['Short_Passing'] +
+                  .05 * p['Long_Shots'])
 
-    p['ovaCF'] = .05 * p['Acceleration'] + \
-                 .05 * p['Sprint_Speed'] + \
-                 .09 * p['Reactions'] + \
-                 .13 * p['Positioning'] + \
-                 .08 * p['Vision'] + \
-                 .15 * p['Ball_Control'] + \
-                 .14 * p['Dribbling'] + \
-                 .11 * p['Finishing'] + \
-                 .02 * p['Heading_Accuracy'] + \
-                 .09 * p['Short_Passing'] + \
-                 .05 * p['Shot_Power'] + \
-                 .04 * p['Long_Shots']
+    p['ovaCF'] = (.05 * p['Acceleration'] +
+                  .05 * p['Sprint_Speed'] +
+                  .09 * p['Reactions'] +
+                  .13 * p['Positioning'] +
+                  .08 * p['Vision'] +
+                  .15 * p['Ball_Control'] +
+                  .14 * p['Dribbling'] +
+                  .11 * p['Finishing'] +
+                  .02 * p['Heading_Accuracy'] +
+                  .09 * p['Short_Passing'] +
+                  .05 * p['Shot_Power'] +
+                  .04 * p['Long_Shots'])
 
-    p['ovaWW'] = .07 * p['Acceleration'] + \
-                 .06 * p['Sprint_Speed'] + \
-                 .03 * p['Agility'] + \
-                 .07 * p['Reactions'] + \
-                 .09 * p['Positioning'] + \
-                 .06 * p['Vision'] + \
-                 .14 * p['Ball_Control'] + \
-                 .09 * p['Crossing'] + \
-                 .16 * p['Dribbling'] + \
-                 .1 * p['Finishing'] + \
-                 .09 * p['Short_Passing'] + \
-                 .04 * p['Long_Shots']
+    p['ovaWW'] = (.07 * p['Acceleration'] +
+                  .06 * p['Sprint_Speed'] +
+                  .03 * p['Agility'] +
+                  .07 * p['Reactions'] +
+                  .09 * p['Positioning'] +
+                  .06 * p['Vision'] +
+                  .14 * p['Ball_Control'] +
+                  .09 * p['Crossing'] +
+                  .16 * p['Dribbling'] +
+                  .1 * p['Finishing'] +
+                  .09 * p['Short_Passing'] +
+                  .04 * p['Long_Shots'])
 
-    p['ovaST'] = .04 * p['Acceleration'] + \
-                 .05 * p['Sprint_Speed'] + \
-                 .05 * p['Strength'] + \
-                 .08 * p['Reactions'] + \
-                 .13 * p['Positioning'] + \
-                 .1 * p['Ball_Control'] + \
-                 .07 * p['Dribbling'] + \
-                 .18 * p['Finishing'] + \
-                 .1 * p['Heading_Accuracy'] + \
-                 .05 * p['Short_Passing'] + \
-                 .1 * p['Shot_Power'] + \
-                 .03 * p['Long_Shots'] + \
-                 .02 * p['Volleys']
+    p['ovaST'] = (.04 * p['Acceleration'] +
+                  .05 * p['Sprint_Speed'] +
+                  .05 * p['Strength'] +
+                  .08 * p['Reactions'] +
+                  .13 * p['Positioning'] +
+                  .1 * p['Ball_Control'] +
+                  .07 * p['Dribbling'] +
+                  .18 * p['Finishing'] +
+                  .1 * p['Heading_Accuracy'] +
+                  .05 * p['Short_Passing'] +
+                  .1 * p['Shot_Power'] +
+                  .03 * p['Long_Shots'] +
+                  .02 * p['Volleys'])
     # not to do a round, because there is no need to do so
     return p
 
 
 # point as an attacker
-def as_attacker(p=[]):
+def as_attacker(p):
     return (calc(p, False)['WW'] + calc(p, False)['CF'] + calc(p, False)['ST']) / 3
 
 
 # point as a midfield
-def as_midfield(p=[]):
+def as_midfield(p):
     return (calc(p, False)['DM'] + calc(p, False)['CM'] + calc(p, False)['AM'] + calc(p, False)['WM']) / 4
 
 
 # point as a defender
-def as_defender(p=[]):
+def as_defender(p):
     return (calc(p, False)['FB'] + calc(p, False)['WB'] + calc(p, False)['CB']) / 3
 
 
-def as_gk(p=[]):
+def as_gk(p):
     return calc(p)['GK']
 
 
@@ -349,8 +349,6 @@ def six_d(p, do_round=True):
           0.1 * p[2] + 0.2 * p[21] + 0.3 * p[25] + 0.3 * p[26] + 0.1 * p[27],
           0.05 * p[16] + 0.25 * p[17] + 0.5 * p[18] + 0.2 * p[20]]
     if do_round:
-        for i in range(6):
-            pt[i] = int(round(pt[i]))
-        return pt
+        return list(map(lambda x: int(round(x)), pt))
     else:
         return pt

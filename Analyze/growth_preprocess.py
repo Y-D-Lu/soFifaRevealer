6e4a89e35f72074e06ed7845c50ae2632fb743ce
Y-D-Lu <e4256318151@gmail.com>
2018-05-11 00:15:35 +0800

    add commit

--- Analyze/growth_preprocess.py
+++ Analyze/growth_preprocess.py
@@ -1,11 +1,14 @@
 from multiprocessing.pool import Pool
-
 import pandas as pd
-
 from Analyze import desc
 from Analyze.point_calc import as_attacker, as_defender, as_midfield
 
+
+# get all players from 2011 to now
 data_all = pd.DataFrame(pd.read_csv('data/all.csv'))
+# get alla for position calc
+alla = desc.alla
+# we use those features to do ML, ova+ means ova next period and growth is itself. Definitely 'ova+'='ova'+'growth'
 comp = ['ID', 'ver', 'Name', 'Weight', 'Height', 'year', 'month', 'date', 'Age', 'Club', 'Nation', 'Pfoot', 'Wfoot',
         'IR', 'SM ', 'ova', 'potential', 'value', 'wage',
         'Crossing', 'Finishing', 'Heading_Accuracy', 'Short_Passing', 'Volleys',
@@ -15,30 +18,36 @@ comp = ['ID', 'ver', 'Name', 'Weight', 'Height', 'year', 'month', 'date', 'Age',
         'Aggression', 'Interceptions', 'Positioning', 'Vision', 'Penalties',
         'Marking', 'Standing_Tackle', 'Sliding_Tackle',
         'GK_Diving', 'GK_Handling', 'GK_Kicking', 'GK_Positioning', 'GK_Reflexes',
-        'pointPAC', 'pointSHO', 'pointPAS', 'pointDRI', 'pointDEF', 'pointPHY',
-        'ova+5', 'growth']
-alla = desc.alla
+        'ova+', 'growth']
 
 
-def switcher(p=[]):
-    li = [as_attacker(p), as_midfield(p), as_defender(p)]
-    return li.index(max(li))
-
-
+# calc the growth and ova+ of players in the period of age
 def growth_calc(start_age=20, end_age=25):
+    # one part of the players are at the age of the start age
     mates_1 = data_all[data_all['Age'] == start_age]
+    # the others are at the end age
     mates_2 = data_all[data_all['Age'] == end_age]
-
-    data_5 = pd.DataFrame(columns=comp)
+    # # only select those whose ova+ is over 70
+    # mates_2 = mates_2[mates_2['ova']>70]
+    # new a df to store those players fit the conditions
+    data_p = pd.DataFrame(columns=comp)
+    # count of players in data_p
     a = 0
-    for i in range(mates_1['ID'].count()):
-        if mates_1.iloc[i, 0] in mates_2['ID'].values:
-            a += 1
-            data_5.loc[a, :] = mates_1.iloc[i, :]
-            data_5['ova+5'][a] = mates_2[mates_2['ID'] == mates_1.iloc[i, 0]]['ova'].values[0]
-            data_5['growth'][a] = data_5['ova+5'][a] - data_5['ova'][a]
 
-    data_5.to_csv('data/' + str(start_age) + 'to' + str(end_age) + '.csv', encoding='utf-8')
+    for i in range(mates_1['ID'].count()):
+        # if player is recorded at both ages
+        if mates_1.iloc[i, 0] in mates_2['ID'].values:
+            # count of player ++
+            a += 1
+            # append player_info to data_p
+            data_p.loc[a, :] = mates_1.iloc[i, :]
+            # set ova+ as ova at end_age
+            data_p['ova+'][a] = mates_2[mates_2['ID'] == mates_1.iloc[i, 0]]['ova'].values[0]
+            # growth=ova+-ova
+            data_p['growth'][a] = data_p['ova+'][a] - data_p['ova'][a]
+    return data_p
+
+    # data_5.to_csv('data/' + str(start_age) + 'to' + str(end_age) + '.csv', encoding='utf-8', index=None)
 
     # data_5.to_csv('data/data_5.csv', encoding='utf-8')
     #
@@ -66,13 +75,30 @@ def growth_calc(start_age=20, end_age=25):
     # data_def.to_csv('data/def.csv', encoding='utf-8')
 
 
+# use multi_task to speed up
 def multi_task(name, sa, ea):
-    growth_calc(sa, ea)
+    return growth_calc(sa, ea)
+
+
+# calc all the players' growth and ova+ every year in the period from start to end
+def calc_every_year(start_age=20, end_age=25):
+    data_e = pd.DataFrame(columns=comp)
+    p = Pool(8)
+    res=[]
+    for i in range(start_age, end_age, 1):
+        res.append(p.apply_async(multi_task, args=(i, i, i + 1)))
+    for i in res:
+        data_e=data_e.append(i.get())
+    p.close()
+    p.join()
+    return data_e
+
 
 
 if __name__ == '__main__':
-    p = Pool(8)
-    for i in range(18, 26, 1):
-        p.apply_async(multi_task, args=(i, i, i + 1))
-    p.close()
-    p.join()
+    # calc every year growths form 18 to 25
+    df = calc_every_year(18,25)
+    df.to_csv('data/annual_18to25.csv', encoding='utf-8', index=None)
+    print(df)
+    # # or just calc 20 to 25
+    # df = growth_calc(20,25)

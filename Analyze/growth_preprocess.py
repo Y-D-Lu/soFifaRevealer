6e2ffa0eb690cf004f72e15f6b7158b7b3aab927
Y-D-Lu <e4256318151@gmail.com>
2018-05-11 00:33:54 +0800

    modify the distinguishing

--- Analyze/growth_preprocess.py
+++ Analyze/growth_preprocess.py
@@ -1,8 +1,7 @@
 from multiprocessing.pool import Pool
 import pandas as pd
 from Analyze import desc
-from Analyze.point_calc import as_attacker, as_defender, as_midfield
-
+from Analyze.point_calc import df_calc
 
 # get all players from 2011 to now
 data_all = pd.DataFrame(pd.read_csv('data/all.csv'))
@@ -47,33 +46,6 @@ def growth_calc(start_age=20, end_age=25):
             data_p['growth'][a] = data_p['ova+'][a] - data_p['ova'][a]
     return data_p
 
-    # data_5.to_csv('data/' + str(start_age) + 'to' + str(end_age) + '.csv', encoding='utf-8', index=None)
-
-    # data_5.to_csv('data/data_5.csv', encoding='utf-8')
-    #
-    # data_att = pd.DataFrame(columns=comp)
-    # data_mid = pd.DataFrame(columns=comp)
-    # data_def = pd.DataFrame(columns=comp)
-    #
-    # data_5.reset_index()
-    # data5a = data_5[alla]
-    #
-    # a = b = c = 0
-    # for x in range(1, data_5['ID'].count() + 1):
-    #     if switcher(data5a.loc[x, :]) == 0:
-    #         data_att.loc[a, :] = data_5.loc[x, :]
-    #         a += 1
-    #     elif switcher(data5a.loc[x, :]) == 1:
-    #         data_mid.loc[b, :] = data_5.loc[x, :]
-    #         b += 1
-    #     else:
-    #         data_def.loc[c, :] = data_5.loc[x, :]
-    #         c += 1
-    #
-    # data_att.to_csv('data/att.csv', encoding='utf-8')
-    # data_mid.to_csv('data/mid.csv', encoding='utf-8')
-    # data_def.to_csv('data/def.csv', encoding='utf-8')
-
 
 # use multi_task to speed up
 def multi_task(name, sa, ea):
@@ -84,15 +56,23 @@ def multi_task(name, sa, ea):
 def calc_every_year(start_age=20, end_age=25):
     data_e = pd.DataFrame(columns=comp)
     p = Pool(8)
-    res=[]
+    res = []
     for i in range(start_age, end_age, 1):
         res.append(p.apply_async(multi_task, args=(i, i, i + 1)))
     for i in res:
-        data_e=data_e.append(i.get())
+        data_e = data_e.append(i.get())
     p.close()
     p.join()
     return data_e
 
+
+# distinguish attacker and defender for next analyzing
+def distinguish_player(df):
+    df_p = df_calc(df)
+    # player whose ova at ATT > DEF suggests an attacker, otherwise a defender
+    df_a = df[df_p['ovaST'] + df_p['ovaWW'] + df_p['ovaCF'] > df_p['ovaCB'] + df_p['ovaFB'] + df_p['ovaWB']]
+    df_d = df[df_p['ovaST'] + df_p['ovaWW'] + df_p['ovaCF'] <= df_p['ovaCB'] + df_p['ovaFB'] + df_p['ovaWB']]
+    return [df_a,df_d]
 
 
 if __name__ == '__main__':
@@ -100,5 +80,9 @@ if __name__ == '__main__':
     df = calc_every_year(18,25)
     df.to_csv('data/annual_18to25.csv', encoding='utf-8', index=None)
     print(df)
-    # # or just calc 20 to 25
-    # df = growth_calc(20,25)
+    # or just calc 20 to 25
+    df = growth_calc(20,25)
+    # df = pd.DataFrame(pd.read_csv('data/annual_18to25.csv'))
+    list_df = distinguish_player(df)
+    list_df[0].to_csv('data/annual_18to25_att.csv', encoding='utf-8', index=None)
+    list_df[1].to_csv('data/annual_18to25_def.csv', encoding='utf-8', index=None)
